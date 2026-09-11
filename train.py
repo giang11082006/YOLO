72375f2b9836752a4f0409bdcc4ac10ff2756479
PMG1108 <giang110802006@gm06gmailcomail.mailcomailcom>
2026-09-11 22:29:58 +0700

    Ignore datasets and videos

--- train.py
+++ train.py
@@ -8,12 +8,4 @@ model.train(
     imgsz=640,
     batch=16
 )
-# Detect trên video
-# results = model2.predict(
-#     source="11964619_2160_3840_30fps.mp4",
-#     save=True,
-#     conf=0.25,
-
-# )
-
 print("done")
